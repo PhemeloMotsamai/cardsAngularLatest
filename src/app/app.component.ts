e3f9d6945f33219b97e1ef3d1ed62fa0fd1b8673
PhemeloMotsamai <phemelojmotsamai@gmail.com>
2023-04-24 09:37:34 +0200

    added property binding syntax and intepolation for cards and component class, updated card contents on cards compontent from parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this tree today'
+
+    },
+    {
+      title: 'Snowy Mountatin',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Lorem pellentesque vel, ullamcorper sit amet nulla.'
+
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'bikini122222',
+      content: 'Pellentesque sit amet lorem quis quam bibendum vulputate. Ut rutrum risus nunc'
+
+    }
+  ];
 }
